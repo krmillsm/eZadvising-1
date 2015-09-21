6a1f1c74353291648f35200237b970dbc41272a4
Phillip Wall <tsyshadow@gmail.com>
2015-09-21 17:43:40 -0400

    Added accessor methods to Requirement class

--- obj/Requirment.php
+++ obj/Requirment.php
@@ -38,18 +38,22 @@ class Requirment
     /**
      * @var int
      */
-    private $minGrade;
+    private $grade;
     /**
      * @var int
      */
     private $catalogYear;
+    /**
+     * @var null
+     */
+    private $courseOptions;
 
 
     /**
      * Requirment constructor.
      */
     public function __construct($id=0, $title="", $category="", $programid=0, $groupid=0, $hours=0, $minGrade=0,
-                                $catalogYear=0)
+                                $catalogYear=0, $courses=[])
     {
 
         $this->id = $id;
@@ -58,12 +62,155 @@ class Requirment
         $this->programid = $programid;
         $this->groupid = $groupid;
         $this->hours = $hours;
-        $this->minGrade = $minGrade;
+        $this->grade = $minGrade;
         $this->catalogYear = $catalogYear;
-
+        $this->courseOptions = $courses;
     }
 
-    public function resolveSufficientRecords($records) {
-
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
     }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param string $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * @param string $category
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+    }
+
+    /**
+     * @return int
+     */
+    public function getProgramid()
+    {
+        return $this->programid;
+    }
+
+    /**
+     * @param int $programid
+     */
+    public function setProgramid($programid)
+    {
+        $this->programid = $programid;
+    }
+
+    /**
+     * @return int
+     */
+    public function getGroupid()
+    {
+        return $this->groupid;
+    }
+
+    /**
+     * @param int $groupid
+     */
+    public function setGroupid($groupid)
+    {
+        $this->groupid = $groupid;
+    }
+
+    /**
+     * @return int
+     */
+    public function getHours()
+    {
+        return $this->hours;
+    }
+
+    /**
+     * @param int $hours
+     */
+    public function setHours($hours)
+    {
+        $this->hours = $hours;
+    }
+
+    /**
+     * @return int
+     */
+    public function getGrade()
+    {
+        return $this->grade;
+    }
+
+    /**
+     * @param int $grade
+     */
+    public function setGrade($grade)
+    {
+        $this->grade = $grade;
+    }
+
+    /**
+     * @return int
+     */
+    public function getCatalogYear()
+    {
+        return $this->catalogYear;
+    }
+
+    /**
+     * @param int $catalogYear
+     */
+    public function setCatalogYear($catalogYear)
+    {
+        $this->catalogYear = $catalogYear;
+    }
+
+    /**
+     * @return null
+     */
+    public function getCourseOptions()
+    {
+        return $this->courseOptions;
+    }
+
+    /**
+     * @param null $courseOptions
+     */
+    public function setCourseOptions($courseOptions)
+    {
+        $this->courseOptions = $courseOptions;
+    }
+
+
+
 }
