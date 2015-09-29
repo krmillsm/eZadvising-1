2eca80a8586887c93ee0de39ddc426179116d63f
Phillip Wall <tsyshadow@gmail.com>
2015-09-29 19:32:14 -0400

    Fixed problem where catalogYear wasn't considered when getting requirements

--- obj/RequirementsDBList.php
+++ obj/RequirementsDBList.php
@@ -15,23 +15,26 @@ class RequirementsDBList
     private $clist;
     private $reclist;
     private $programId;
+    private $year;
 
     /**
      * RequirementsDBList constructor.
      */
-    public function __construct(PDO $con, $clist, $reclist, $programId)
+    public function __construct(PDO $con, $clist, $reclist, $programId, $year)
     {
         $this->con = $con;
         $this->clist = $clist;
         $this->reclist = $reclist;
         $this->programId = $programId;
+        $this->year = $year;
     }
 
     public function getRequirements() {
-        $sql = "Select * from program_requirements where programId=:id";
+        $sql = "Select * from program_requirements where programId=:id and catalogYear=:year";
 
         $stmt = $this->con->prepare($sql);
         $stmt->bindValue(":id", $this->programId);
+        $stmt->bindValue(":year", $this->year);
         $stmt->execute();
         $results = $stmt->fetchAll();
 
@@ -41,7 +44,7 @@ class RequirementsDBList
             $req = new \obj\Requirment($row['id'], $row['title'], $row['category'], $row['programId'], $row['groupId'],
                 $row['numCreditHours'], $row['minGrade'], $row['catalogYear']);
 
-            $req->setCourseOptions($this->getCoursesForProgram($row['groupId']));
+            $req->setCourseOptions($this->getCoursesForRequirement($row['groupId']));
             $req->setCoursesCounting($this->reclist->getCompletedRecordsForRequirement($req));
             $req->setCoursesCountingPlanned($this->reclist->getPendingRecordsForRequirement($req));
             $ret[]=$req;
@@ -51,11 +54,11 @@ class RequirementsDBList
 
     }
 
-    private function getCoursesForProgram($programId) {
+    private function getCoursesForRequirement($groupId) {
         $sql = "Select courseId from course_groups where groupId=:id";
 
         $stmt = $this->con->prepare($sql);
-        $stmt->bindValue(":id", $programId);
+        $stmt->bindValue(":id", $groupId);
         $stmt->execute();
         $results = $stmt->fetchAll();
 
